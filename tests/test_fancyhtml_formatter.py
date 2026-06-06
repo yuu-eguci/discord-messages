601d5091ff0772d34a733093cfa640e1aacf121b
yuu-eguci <yuu.eguci@gmail.com>
2026-06-06 14:34:03 +0900

    Create test_fancyhtml_formatter.py

---
+++ tests/test_fancyhtml_formatter.py
@@ -0,0 +1,29 @@
+import unittest
+
+from formatters.fancyhtml import format_fancyhtml
+
+
+class FancyHtmlFormatterTests(unittest.TestCase):
+    def test_renders_clickable_sections_with_children(self) -> None:
+        html = format_fancyhtml(
+            [
+                [
+                    {
+                        "heading": "foo & bar",
+                        "children": [
+                            "baz <qux>",
+                        ],
+                    },
+                ],
+            ],
+        )
+
+        self.assertIn("<!doctype html>", html)
+        self.assertIn("<details class=\"entry\">", html)
+        self.assertIn("<summary>foo &amp; bar</summary>", html)
+        self.assertIn("<li>baz &lt;qux&gt;</li>", html)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
